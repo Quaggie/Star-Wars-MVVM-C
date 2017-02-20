ae7e071c201877377fa2a863f94b90375a9fdd12
Jonathan Bijos <jonathanpbijos@gmail.com>
2017-02-20 09:24:49 -0300

    Initial Commit

---
+++ Star Wars MVVM-C/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Star Wars MVVM-C
+//
+//  Created by Jonathan Bijos on 20/02/17.
+//  Copyright © 2017 quaggie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
